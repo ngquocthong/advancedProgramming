3e9e59ac6ce1595ebe354904ad7ce6913341a2f1
unknown <thongnguyennqt@gmail.com>
2022-11-07 10:26:55 +0700

    first commit

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+using System.Globalization;
+
+Console.WriteLine("Input: ");
+string input = Console.ReadLine();
+DateTime date = DateTime.ParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture);
+Console.WriteLine(date.DayOfWeek);
+*/
+
+string[] words = Console.ReadLine().Split(' ');
+Random r = new Random();
+int random = r.Next(words.Length);
+
+for(int pos1 = 0; pos1 < words.Length; pos1++)
+{
+    int pos2 = r.Next(words.Length);
+    String tmp = words[pos1];
+    words[pos1] = words[pos2];
+    words[pos2] = tmp;
+}
+Console.WriteLine(string.Join(Environment.NewLine, words));
